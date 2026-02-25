dad1a1882d9e320f505c1895642fbd620afaf88c
RyoYamada <168729373+ryo-yamada@users.noreply.github.com>
2026-02-25 17:00:05 +0000

    Tiny character correction

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if len(sys.argv) != 2:
 
 target = socket.gethostbyname(sys.argv[1])
 
-print("hikimap 1.0 - (github.com/ryo-yamada/hikimap)")
+print("hikimap 1.1 - (github.com/ryo-yamada/hikimap)")
 print("* Service guesses are TCP only")
 print(f"Target - {target}")
 print(f"Started - {datetime.now().strftime('%H:%M:%S')}")
